e6718271dfe3ebb167b238d0d3f78266517ebd42
Sydney Bao <sydneybao@google.com>
2025-07-21 01:59:17 -0700

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = 'AIzaSyAR9rKkEUCTi6F3bJps - Mtp093hPbytzoA';
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
